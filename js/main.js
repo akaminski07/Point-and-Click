6e4a195c295ac904a10b23e7347097cda9ef4f37
akaminski07 <gameradrianox@gmail.com>
2024-02-19 11:38:48 +0100

    Update
    - Added a key
    - Added a new door
    - Inventory updates

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@ document.getElementById("title").innerText = "Point and Click Adventure game";
 //Game window reference
 const gameWindow = document.getElementById("gameWindow");
 
+//Game state
+gameState = {
+    "door2locked": true,
+}
+
 //Main Character
 const mainCharacter = document.getElementById("mainCharacter");
 const offsetCharacter = 16;
+
+//Inventory
+const inventoryBox = document.getElementById("inventoryBox");
+const inventoryList = document.getElementById("inventoryList");
 
 const door1 = document.getElementById("door1");
 const sign = document.getElementById("sign");
@@ -20,10 +29,35 @@ gameWindow.onclick = function (e) {
     mainCharacter.style.top = y - offsetCharacter + "px";
 
     switch (e.target.id) {
+
+
         case "door1":
             mainCharacter.style.backgroundColor = "#FFFF00";
             door1.style.opacity = 0.5;
             sign.style.opacity = 1;
+            if (document.getElementById("key1") !== null) {
+                console.log('Found key!');
+                document.getElementById("key1").remove();
+                const keyElement = document.createElement("li");
+                keyElement.id = "inv-key";
+                keyElement.innerText = "Key";
+                inventoryList.appendChild(keyElement);
+            }
+
+            break;
+        case "door2":
+            if (gameState.door2locked == true) {
+                if (document.getElementById("inv-key") !== null) {
+                    gameState.door2locked = false;
+                    console.log('Door unlocked!')
+                } else {
+                    alert("Door is locked!");
+                }
+            } else {
+                console.log('enter building');
+            }
+
+
             break;
 
         case "sign":
